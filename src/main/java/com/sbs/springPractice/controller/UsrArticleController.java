ea0f61532a90a7f0b716c3c5ca2fbcd51c7ac5dc
qbj700 <qbj700@gmail.com>
2021-02-24 14:29:45 +0900

    Util.mapOf 적용하여 map형식 데이터를 자동생성 할수있도록 구현

--- src/main/java/com/sbs/springPractice/controller/UsrArticleController.java
+++ src/main/java/com/sbs/springPractice/controller/UsrArticleController.java
@@ -18,111 +18,89 @@ import com.sbs.springPractice.util.Util;
 public class UsrArticleController {
 	private int articlesLastId;
 	private List<Article> articles;
-	
+
 	public UsrArticleController() {
-		
+
 		articlesLastId = 0;
 		articles = new ArrayList<>();
-		
+
 		articles.add(new Article(++articlesLastId, "2020-12-12 12:12:12", "2020-12-12 12:12:12", "제목1", "내용1"));
 		articles.add(new Article(++articlesLastId, "2020-12-12 12:12:12", "2020-12-12 12:12:12", "제목2", "내용2"));
 	}
-	
+
 	@RequestMapping("/usr/article/detail")
 	@ResponseBody
 	public Article showDetail(int id) {
 		return articles.get(id - 1);
 	}
-	
+
 	@RequestMapping("/usr/article/list")
 	@ResponseBody
 	public List<Article> showList() {
 		return articles;
 	}
-	
+
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Map<String, Object> doAdd(String title, String body) {
-		
-		
+
 		String regDate = Util.getNowDateStr();
 		String updateDate = Util.getNowDateStr();
-		
+
 		articles.add(new Article(++articlesLastId, regDate, updateDate, title, body));
-		
-		Map<String, Object> rs = new HashMap<>();
-		rs.put("resultCode", "S-1");
-		rs.put("msg", "성공하였습니다.");
-		rs.put("id", articlesLastId);
-		
-		return rs;
-		
+
+		return Util.mapOf("resultCode", "S-1", "msg", "성공하였습니다.", "id", articlesLastId);
+
 	}
 
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
 	public Map<String, Object> doDelete(int id) {
 		boolean deleteArticleRs = deleteArticle(id);
-		
-		Map<String, Object> rs = new HashMap<>();
-		
-		if ( deleteArticleRs ) {
-			rs.put("resultCode", "S-1");
-			rs.put("msg", "성공하였습니다.");
-			rs.put("id", id);
+
+
+		if (deleteArticleRs == false) {
+			return Util.mapOf("resultCode", "F-1", "msg", "해당 게시물은 존재하지 않습니다.", "id", id);
 		}
-		else {
-			rs.put("resultCode", "F-1");
-			rs.put("msg", "해당 게시물은 존재하지 않습니다.");
-			rs.put("id", id);
-		}
-		
-		
-		return rs;
-		
+
+		return Util.mapOf("resultCode", "S-1", "msg", "성공하였습니다.", "id", id);
+
 	}
 
 	private boolean deleteArticle(int id) {
-		for ( Article article : articles) {
-			if ( article.getId() == id) {
+		for (Article article : articles) {
+			if (article.getId() == id) {
 				articles.remove(article);
 				return true;
 			}
 		}
 		return false;
-		
+
 	}
-	
+
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
 	public Map<String, Object> doModify(int id, String title, String body) {
 		Article selArticle = null;
-		
-		for ( Article article : articles ) {
-			if ( article.getId() == id) {
+
+		for (Article article : articles) {
+			if (article.getId() == id) {
 				selArticle = article;
 				break;
 			}
 		}
-		
-		Map<String, Object> rs = new HashMap<>();
-		
-		if ( selArticle == null ) {
-			rs.put("resultCode", "F-1");
-			rs.put("msg", String.format("%d번 게시물은 존재하지않습니다.", id));
-			return rs;
+
+		if (selArticle == null) {
+			return Util.mapOf("resultCode", "F-1", "msg", String.format("%d번 게시물은 존재하지않습니다.", id));
 		}
-		
+
 		selArticle.setTitle(title);
 		selArticle.setBody(body);
-		
+
 		String updateDate = Util.getNowDateStr();
 		selArticle.setUpdateDate(updateDate);
-		
-		rs.put("resultCode", "S-1");
-		rs.put("msg", String.format("%d번 게시물이 수정되었습니다.", id));
-		
-		return rs;
+
+		return Util.mapOf("resultCode", "S-1", "msg", String.format("%d번 게시물이 수정되었습니다.", id));
 	}
 
 }
