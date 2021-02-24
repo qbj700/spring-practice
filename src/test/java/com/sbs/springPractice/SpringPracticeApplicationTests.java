5cad89e1e9ea863f70c506085f5615809e0c3214
qbj700 <qbj700@gmail.com>
2021-02-24 11:19:21 +0900

    첫 커밋

---
+++ src/test/java/com/sbs/springPractice/SpringPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbs.springPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
