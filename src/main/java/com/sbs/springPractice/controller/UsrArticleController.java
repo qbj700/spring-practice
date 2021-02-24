c734a87f1d103f5b58b35fd352443a14af1e354d
qbj700 <qbj700@gmail.com>
2021-02-24 12:36:03 +0900

    게시물 작성기능 구현

--- src/main/java/com/sbs/springPractice/controller/UsrArticleController.java
+++ src/main/java/com/sbs/springPractice/controller/UsrArticleController.java
@@ -1,7 +1,9 @@
 package com.sbs.springPractice.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +13,16 @@ import com.sbs.springPractice.dto.Article;
 
 @Controller
 public class UsrArticleController {
+	private int articlesLastId;
 	private List<Article> articles;
 	
 	public UsrArticleController() {
+		
+		articlesLastId = 0;
 		articles = new ArrayList<>();
 		
-		articles.add(new Article(1, "2020-12-12 12:12:12", "2020-12-12 12:12:12", "제목1", "내용1"));
-		articles.add(new Article(2, "2020-12-12 12:12:12", "2020-12-12 12:12:12", "제목2", "내용2"));
+		articles.add(new Article(++articlesLastId, "2020-12-12 12:12:12", "2020-12-12 12:12:12", "제목1", "내용1"));
+		articles.add(new Article(++articlesLastId, "2020-12-12 12:12:12", "2020-12-12 12:12:12", "제목2", "내용2"));
 	}
 	
 	@RequestMapping("/usr/article/detail")
@@ -30,6 +35,20 @@ public class UsrArticleController {
 	@ResponseBody
 	public List<Article> showList() {
 		return articles;
+	}
+	
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Map<String, Object> doAdd(String regDate, String updateDate, String title, String body) {
+		articles.add(new Article(++articlesLastId, regDate, updateDate, title, body));
+		
+		Map<String, Object> rs = new HashMap<>();
+		rs.put("resultCode", "S-1");
+		rs.put("msg", "성공하였습니다.");
+		rs.put("id", articlesLastId);
+		
+		return rs;
+		
 	}
 
 }
