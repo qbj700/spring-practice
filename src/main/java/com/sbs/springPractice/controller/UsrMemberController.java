26e1e0e890cdb1c9a53d7f95b6195fa74c3b5a35
qbj700 <qbj700@gmail.com>
2021-02-25 15:50:15 +0900

    회원가입시 로그인 아이디 중복체크

--- src/main/java/com/sbs/springPractice/controller/UsrMemberController.java
+++ src/main/java/com/sbs/springPractice/controller/UsrMemberController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sbs.springPractice.dto.Member;
 import com.sbs.springPractice.dto.ResultData;
 import com.sbs.springPractice.service.MemberService;
 
@@ -22,6 +23,13 @@ public class UsrMemberController {
 		if (param.get("loginId") == null) {
 			return new ResultData("F-1", "loginId를 입력해주세요.");
 		}
+		
+		Member existingMember = memberService.getMemberByLoginId((String) param.get("loginId"));
+		
+		if (existingMember != null) {
+			return new ResultData("F-2", String.format("%s (은)는 이미 사용중인 로그인아이디 입니다.", param.get("loginId")));
+		}
+		
 		if (param.get("loginPw") == null) {
 			return new ResultData("F-1", "loginPw를 입력해주세요.");
 		}
--- src/main/java/com/sbs/springPractice/dao/MemberDao.java
+++ src/main/java/com/sbs/springPractice/dao/MemberDao.java
@@ -3,10 +3,17 @@ package com.sbs.springPractice.dao;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.sbs.springPractice.dto.Member;
 
 @Mapper
 public interface MemberDao {
 
 	public void join(Map<String, Object> param);
+	
+	public Member getMember(@Param("id") int id);
+
+	public Member getMemberByLoginId(@Param("loginId") String loginId);
 
 }
