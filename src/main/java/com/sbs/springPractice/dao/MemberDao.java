1834228f432217e37f97dae0bea345873c2e0dc7
qbj700 <qbj700@gmail.com>
2021-02-25 15:39:28 +0900

    회원가입 구현

---
+++ src/main/java/com/sbs/springPractice/dao/MemberDao.java
@@ -0,0 +1,12 @@
+package com.sbs.springPractice.dao;
+
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MemberDao {
+
+	public void join(Map<String, Object> param);
+
+}
---
+++ src/main/java/com/sbs/springPractice/service/MemberService.java
@@ -0,0 +1,25 @@
+package com.sbs.springPractice.service;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sbs.springPractice.dao.MemberDao;
+import com.sbs.springPractice.dto.ResultData;
+import com.sbs.springPractice.util.Util;
+
+@Service
+public class MemberService {
+	@Autowired
+	private MemberDao memberDao;
+
+	public ResultData join(Map<String, Object> param) {
+		memberDao.join(param);
+
+		int id = Util.getAsInt(param.get("id"), 0);
+
+		return new ResultData("S-1", String.format("%s님 환영합니다.", param.get("nickname")), "id", id);
+	}
+
+}
