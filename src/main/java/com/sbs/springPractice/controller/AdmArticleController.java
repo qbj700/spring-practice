accec9bd7d2d1f84ffb953a9d5d37b34fb64f2db
qbj700 <qbj700@gmail.com>
2021-03-02 23:22:36 +0900

    관리자 게시물 작성 폼에 파일입력상자 2개 추가

--- src/main/java/com/sbs/springPractice/controller/AdmArticleController.java
+++ src/main/java/com/sbs/springPractice/controller/AdmArticleController.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartRequest;
 
 import com.sbs.springPractice.dto.Article;
 import com.sbs.springPractice.dto.Board;
@@ -26,7 +28,6 @@ public class AdmArticleController extends BaseController {
 	@RequestMapping("/adm/article/detail")
 	@ResponseBody
 	public ResultData showDetail(Integer id) {
-		
 		if (id == null) {
 			return new ResultData("F-1", "id를 입력해주세요.");
 		}
@@ -36,15 +37,15 @@ public class AdmArticleController extends BaseController {
 		if (article == null) {
 			return new ResultData("F-2", "존재하지 않는 게시물번호 입니다.");
 		}
-		
+
 		return new ResultData("S-1", "성공", "article", article);
 	}
 
 	@RequestMapping("/adm/article/list")
 	public String showList(HttpServletRequest req, @RequestParam(defaultValue = "1") int boardId, String searchKeywordType, String searchKeyword, @RequestParam(defaultValue = "1") int page) {
-		
-		Board board = articleService.getBoard(boardId);
 
+		Board board = articleService.getBoard(boardId);
+		
 		if ( board == null ) {
 			return msgAndBack(req, "존재하지 않는 게시판 입니다.");
 		}
@@ -52,55 +53,29 @@ public class AdmArticleController extends BaseController {
 		if (searchKeywordType != null) {
 			searchKeywordType = searchKeywordType.trim();
 		}
-		
-		if ( searchKeywordType == null || searchKeywordType.length() == 0) {
+
+		if (searchKeywordType == null || searchKeywordType.length() == 0) {
 			searchKeywordType = "titleAndBody";
 		}
-		
-		if ( searchKeyword != null && searchKeyword.length() == 0 ) {
+
+		if (searchKeyword != null && searchKeyword.length() == 0) {
 			searchKeyword = null;
 		}
-		
-		if ( searchKeyword != null ) {
+
+		if (searchKeyword != null) {
 			searchKeyword = searchKeyword.trim();
 		}
-		
-		if ( searchKeyword == null) {
+
+		if (searchKeyword == null) {
 			searchKeywordType = null;
 		}
 		
 		int itemsInAPage = 20;
-		
+
 		List<Article> articles = articleService.getForPrintArticles(boardId, searchKeywordType, searchKeyword, page, itemsInAPage);
 		req.setAttribute("articles", articles);
 
 		return "adm/article/list";
-	}
-	
-	@RequestMapping("/adm/article/add")
-	public String showAdd(@RequestParam Map<String, Object> param, HttpServletRequest req) {
-		return "adm/article/add";
-	}
-
-	@RequestMapping("/adm/article/doAdd")
-	@ResponseBody
-	public ResultData doAdd(@RequestParam Map<String, Object> param, HttpServletRequest req) {
-		int loginedMemberId = (int)req.getAttribute("loginedMemberId");
-
-		if (param.get("title") == null) {
-			return new ResultData("F-1", "title을 입력해주세요.");
-		}
-		if (param.get("body") == null) {
-			return new ResultData("F-1", "body를 입력해주세요.");
-		}
-		if (param.get("boardId") == null) {
-			return new ResultData("F-1", "boardId를 입력해주세요.");
-		}
-		
-		param.put("memberId", loginedMemberId);
-
-		return articleService.addArticle(param);
-
 	}
 	
 	@RequestMapping("/adm/article/doAddReply")
@@ -115,18 +90,49 @@ public class AdmArticleController extends BaseController {
 		if (param.get("articleId") == null) {
 			return new ResultData("F-1", "articleId를 입력해주세요.");
 		}
-		
+
 		param.put("memberId", loginedMemberId);
 
 		return articleService.addReply(param);
+	}
+	
+	@RequestMapping("/adm/article/add")
+	public String showAdd(@RequestParam Map<String, Object> param, HttpServletRequest req) {
+		return "adm/article/add";
+	}
 
+	@RequestMapping("/adm/article/doAdd")
+	@ResponseBody
+	public ResultData doAdd(@RequestParam Map<String, Object> param, HttpServletRequest req,
+			MultipartRequest multipartRequest) {
+		Map<String, MultipartFile> fileMap = multipartRequest.getFileMap();
+		
+		if ( true ) {
+			return new ResultData("S-1", "테스트", "fileMap.keySet", fileMap.keySet());			
+		}
+		
+		int loginedMemberId = (int)req.getAttribute("loginedMemberId");
+		
+		if (param.get("title") == null) {
+			return new ResultData("F-1", "title을 입력해주세요.");
+		}
+
+		if (param.get("body") == null) {
+			return new ResultData("F-1", "body를 입력해주세요.");
+		}
+		
+		
+
+		param.put("memberId", loginedMemberId);
+
+		return articleService.addArticle(param);
 	}
 
 	@RequestMapping("/adm/article/doDelete")
 	@ResponseBody
 	public ResultData doDelete(Integer id, HttpServletRequest req) {
 		int loginedMemberId = (int)req.getAttribute("loginedMemberId");
-				
+
 		if (id == null) {
 			return new ResultData("F-1", "id를 입력해주세요.");
 		}
@@ -134,49 +140,47 @@ public class AdmArticleController extends BaseController {
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
-			return new ResultData("F-1", String.format("%d번 게시물은 존재하지않습니다.", id));
-		}
-		
-		ResultData getActorCanDeleteRd = articleService.getActorCanDeleteRd(article, loginedMemberId);
-		
-		if ( getActorCanDeleteRd.isFail()) {
-			return getActorCanDeleteRd;
+			return new ResultData("F-1", "해당 게시물은 존재하지 않습니다.");
 		}
 
+		ResultData actorCanDeleteRd = articleService.getActorCanDeleteRd(article, loginedMemberId);
+
+		if (actorCanDeleteRd.isFail()) {
+			return actorCanDeleteRd;
+		}
 
 		return articleService.deleteArticle(id);
 	}
 
 	@RequestMapping("/adm/article/doModify")
 	@ResponseBody
-	public ResultData doModify(Integer id, String title, String body, HttpSession session) {
-		int loginedMemberId = Util.getAsInt(session.getAttribute("loginedMemberId"), 0);
-		
+	public ResultData doModify(Integer id, String title, String body, HttpServletRequest req) {
+		int loginedMemberId = (int)req.getAttribute("loginedMemberId");
+
 		if (id == null) {
 			return new ResultData("F-1", "id를 입력해주세요.");
 		}
-		
+
+		if (title == null) {
+			return new ResultData("F-1", "title을 입력해주세요.");
+		}
+
+		if (body == null) {
+			return new ResultData("F-1", "body를 입력해주세요.");
+		}
+
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
-			return new ResultData("F-1", String.format("%d번 게시물은 존재하지않습니다.", id));
+			return new ResultData("F-1", "해당 게시물은 존재하지 않습니다.");
 		}
-		
-		if (title == null) {
-			title = article.getTitle();
-		}
-		
-		if (body == null) {
-			body = article.getBody();
-		}
-		
-		ResultData getActorCanModifyRd = articleService.getActorCanModifyRd(article, loginedMemberId);
-		
-		if ( getActorCanModifyRd.isFail()) {
-			return getActorCanModifyRd;
+
+		ResultData actorCanModifyRd = articleService.getActorCanModifyRd(article, loginedMemberId);
+
+		if (actorCanModifyRd.isFail()) {
+			return actorCanModifyRd;
 		}
 
 		return articleService.modifyArticle(id, title, body);
 	}
-
 }
