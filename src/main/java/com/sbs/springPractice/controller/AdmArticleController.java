34246922f38824fbc71df5c1769afd5d066b924c
qbj700 <qbj700@gmail.com>
2021-03-02 23:01:04 +0900

    관리자 게시물 작성 폼 구현

--- src/main/java/com/sbs/springPractice/controller/AdmArticleController.java
+++ src/main/java/com/sbs/springPractice/controller/AdmArticleController.java
@@ -76,6 +76,11 @@ public class AdmArticleController extends BaseController {
 
 		return "adm/article/list";
 	}
+	
+	@RequestMapping("/adm/article/add")
+	public String showAdd(@RequestParam Map<String, Object> param, HttpServletRequest req) {
+		return "adm/article/add";
+	}
 
 	@RequestMapping("/adm/article/doAdd")
 	@ResponseBody
