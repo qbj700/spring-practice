60efc3e53120872ee6225aceef23ed81c8b30983
qbj700 <qbj700@gmail.com>
2021-03-04 19:53:39 +0900

    swagger 문서 설정

--- src/main/java/com/sbs/springPractice/config/SwaggerConfig.java
+++ src/main/java/com/sbs/springPractice/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package com.sbs.springPractice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,14 +15,23 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-	
+
 	@Bean
 	public Docket api() {
-		return new Docket (DocumentationType.SWAGGER_2)
+		return new Docket(DocumentationType.SWAGGER_2)
+				.useDefaultResponseMessages(false)
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.sbs.springPractice.controller"))
 				.paths(PathSelectors.any())
-				.build();
+				.build()
+				.apiInfo(apiInfo());
 	}
 
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Spring-API")
+				.description("[Spring-practice] 를 이용한 API 명세서")
+				.contact(new Contact("Spring-practice", "https://github.com/qbj700/spring-practice", "qbj700@gmail.com"))
+				.version("1.0").build();
+	}
 }
